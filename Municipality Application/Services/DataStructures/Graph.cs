c2baa9524d09eef062c49ef2b5a05f22705a8577
ST10257863 <ST10257863@vcconnect.edu.za>
2025-11-11 13:55:19 +0200

    Scaffolding: Added classes containing different data strucutres

---
+++ Municipality Application/Services/DataStructures/Graph.cs
@@ -0,0 +1,124 @@
+﻿namespace Municipality_Application.Services.DataStructures
+{
+    /// <summary>
+    /// Represents a generic undirected graph using an adjacency list.
+    /// </summary>
+    /// <typeparam name="T">The type of vertices.</typeparam>
+    public class Graph<T> where T : notnull
+    {
+        private readonly Dictionary<T, List<(T, double)>> _adjacencyList = new();
+
+        /// <summary>
+        /// Adds a vertex to the graph.
+        /// </summary>
+        public void AddVertex(T vertex)
+        {
+            if (!_adjacencyList.ContainsKey(vertex))
+                _adjacencyList[vertex] = new List<(T, double)>();
+        }
+
+        /// <summary>
+        /// Adds an undirected edge between two vertices with an optional weight.
+        /// </summary>
+        public void AddEdge(T from, T to, double weight = 1.0)
+        {
+            AddVertex(from);
+            AddVertex(to);
+            _adjacencyList[from].Add((to, weight));
+            _adjacencyList[to].Add((from, weight));
+        }
+
+        /// <summary>
+        /// Performs a breadth-first search (BFS) starting from the given vertex.
+        /// </summary>
+        public IEnumerable<T> Bfs(T start)
+        {
+            var visited = new HashSet<T>();
+            var queue = new Queue<T>();
+            queue.Enqueue(start);
+            visited.Add(start);
+
+            while (queue.Count > 0)
+            {
+                var vertex = queue.Dequeue();
+                yield return vertex;
+
+                foreach (var (neighbor, _) in _adjacencyList[vertex])
+                {
+                    if (visited.Add(neighbor))
+                        queue.Enqueue(neighbor);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Performs a depth-first search (DFS) starting from the given vertex.
+        /// </summary>
+        public IEnumerable<T> Dfs(T start)
+        {
+            var visited = new HashSet<T>();
+            var stack = new Stack<T>();
+            stack.Push(start);
+
+            while (stack.Count > 0)
+            {
+                var vertex = stack.Pop();
+                if (visited.Add(vertex))
+                {
+                    yield return vertex;
+                    foreach (var (neighbor, _) in _adjacencyList[vertex].AsEnumerable().Reverse())
+                    {
+                        if (!visited.Contains(neighbor))
+                            stack.Push(neighbor);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the minimum spanning tree (MST) using Prim's algorithm.
+        /// </summary>
+        public List<(T from, T to, double weight)> GetMinimumSpanningTree()
+        {
+            var result = new List<(T, T, double)>();
+            if (_adjacencyList.Count == 0)
+                return result;
+
+            var visited = new HashSet<T>();
+            var minHeap = new SortedSet<(double, T, T)>(Comparer<(double, T, T)>.Create((a, b) =>
+            {
+                int cmp = a.Item1.CompareTo(b.Item1);
+                if (cmp != 0) return cmp;
+                cmp = Comparer<T>.Default.Compare(a.Item2, b.Item2);
+                if (cmp != 0) return cmp;
+                return Comparer<T>.Default.Compare(a.Item3, b.Item3);
+            }));
+
+            var start = _adjacencyList.Keys.First();
+            visited.Add(start);
+
+            foreach (var (neighbor, weight) in _adjacencyList[start])
+                minHeap.Add((weight, start, neighbor));
+
+            while (minHeap.Count > 0)
+            {
+                var (weight, from, to) = minHeap.Min;
+                minHeap.Remove(minHeap.Min);
+
+                if (visited.Contains(to))
+                    continue;
+
+                result.Add((from, to, weight));
+                visited.Add(to);
+
+                foreach (var (neighbor, w) in _adjacencyList[to])
+                {
+                    if (!visited.Contains(neighbor))
+                        minHeap.Add((w, to, neighbor));
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Municipality Application/Services/DataStructures/MinHeap.cs
@@ -0,0 +1,83 @@
+﻿namespace Municipality_Application.Services.DataStructures
+{
+    /// <summary>
+    /// Represents a generic min-heap data structure.
+    /// </summary>
+    /// <typeparam name="T">The type of elements stored, must implement IComparable&lt;T&gt;.</typeparam>
+    public class MinHeap<T> where T : IComparable<T>
+    {
+        private readonly List<T> _elements = new();
+
+        /// <summary>
+        /// Gets the number of elements in the heap.
+        /// </summary>
+        public int Count => _elements.Count;
+
+        /// <summary>
+        /// Inserts a value into the min-heap.
+        /// </summary>
+        public void Insert(T value)
+        {
+            _elements.Add(value);
+            HeapifyUp(_elements.Count - 1);
+        }
+
+        /// <summary>
+        /// Returns the minimum element without removing it.
+        /// </summary>
+        public T Peek()
+        {
+            if (_elements.Count == 0)
+                throw new InvalidOperationException("Heap is empty.");
+            return _elements[0];
+        }
+
+        /// <summary>
+        /// Extracts and returns the minimum element from the heap.
+        /// </summary>
+        public T ExtractMin()
+        {
+            if (_elements.Count == 0)
+                throw new InvalidOperationException("Heap is empty.");
+            T min = _elements[0];
+            _elements[0] = _elements[^1];
+            _elements.RemoveAt(_elements.Count - 1);
+            HeapifyDown(0);
+            return min;
+        }
+
+        private void HeapifyUp(int index)
+        {
+            while (index > 0)
+            {
+                int parent = (index - 1) / 2;
+                if (_elements[index].CompareTo(_elements[parent]) >= 0)
+                    break;
+                (_elements[index], _elements[parent]) = (_elements[parent], _elements[index]);
+                index = parent;
+            }
+        }
+
+        private void HeapifyDown(int index)
+        {
+            int lastIndex = _elements.Count - 1;
+            while (true)
+            {
+                int left = 2 * index + 1;
+                int right = 2 * index + 2;
+                int smallest = index;
+
+                if (left <= lastIndex && _elements[left].CompareTo(_elements[smallest]) < 0)
+                    smallest = left;
+                if (right <= lastIndex && _elements[right].CompareTo(_elements[smallest]) < 0)
+                    smallest = right;
+
+                if (smallest == index)
+                    break;
+
+                (_elements[index], _elements[smallest]) = (_elements[smallest], _elements[index]);
+                index = smallest;
+            }
+        }
+    }
+}
